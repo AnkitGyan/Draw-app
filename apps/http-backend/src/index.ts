4b9b901bb233778899f01c103c47b73c83da371e
Ankit Sharma <ankit0525252@gmail.com>
2026-06-12 12:25:18 +0530

    created zod schema and common package for jwt token

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import type { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { Middleware } from "./middleware";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types";
 
 const app = express();
 
@@ -11,22 +13,40 @@ app.get("/", (req: Request, res: Response) => {
 );
 
 app.post("/signup", (req: Request, res: Response)=>{
-  const {name, email, password} = req.body;
+  const data = CreateUserSchema.safeParse(req.body);
+  if(!data.success){
+    return res.json({
+      message: "Incorrect Inputs"
+    })
+  }
+
   res.status(2000).json({
     message: "Signup successfull",
   })
 })
 
 app.post("/login", (req: Request, res: Response)=>{
-  const {email, password} = req.body;
-
+  const data = SigninSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+      message:"Incorrect Inputs",
+    })
+  }
   res.status(2000).json({
     message: "Login successfull",
   })
 });
 
 app.post("/room", Middleware, (req: Request, res:Response)=>{
-
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+      return res.json({
+        message: "Incorrect Inputs"
+      })
+    }
+    res.status(2000).json({
+      message: "Room created successfully",
+    })
 })
 
 app.listen(8000, () => {
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,40 @@
 import { WebSocketServer } from "ws";
+import { RawData } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
 
-const wss = new WebSocketServer({port : 8080});
 
-wss.on("connection", function connection(ws){
-  ws.on("message", function message(data){
-    ws.send(`Hello, you sent -> ${data}`);
-  })
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws, request) => {
+  const url = request.url;
+
+  if (!url) {
+    ws.close(1008, "Invalid URL");
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token");
+
+  if (!token) {
+    ws.close(1008, "Token missing");
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    
+    if(!decoded || !(decoded as JwtPayload).userId){
+      ws.close(1008, "User is not authenticated");
+      return;
+    }
+
+    ws.on("message", (data : RawData) => {
+      ws.send(`Hello, you sent -> ${data}`);
+    });
+  } catch (err) {
+    console.log("Invalid token");
+    ws.close(1008, "Invalid token");
+  }
 });
