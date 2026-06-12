9c43980cd0a41d1886390e7bbd24bce0a5839366
Ankit Sharma <ankit0525252@gmail.com>
2026-06-12 20:33:36 +0530

    added database and prisma

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { Middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types";
+import { Client } from "@repo/db/client";
 
 const app = express();
 
